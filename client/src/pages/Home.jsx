7b9e854618df44125d98727b54f48f4b0e71918c
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-07-24 16:12:18 +0330

    Newsletter Done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
 import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
     </>
   );
 };
