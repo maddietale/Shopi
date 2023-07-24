64f12ce8b1d360f529d9beb8ba493bf23b33c265
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-07-24 15:46:54 +0330

    Products Done

--- client/src/data.js
+++ client/src/data.js
@@ -1,10 +1,10 @@
 export const sliderItems = [
     {
         id: 1,
-        img: "https://i.ibb.co/XsdmR2c/1.png",
-        title: "SUMMER SALE",
+        img: "https://i.ibb.co/cXFnLLV/3.png",
+        title: "LOUNGEWEAR LOVE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: "f5fafd",
+        bg: "fbf0f4",
     },
     {
         id: 2,
@@ -15,10 +15,10 @@ export const sliderItems = [
     },
     {
         id: 3,
-        img: "https://i.ibb.co/cXFnLLV/3.png",
-        title: "LOUNGEWEAR LOVE",
+        img: "https://i.ibb.co/XsdmR2c/1.png",
+        title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: "fbf0f4",
+        bg: "f5fafd",
     },
 ];
 
