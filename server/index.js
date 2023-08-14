3378489ad13f56eae408e8549525fa1c9f3959d6
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-08-14 16:45:02 +0330

    setup models and routes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-
+// ROUTES
+const userRoute = require("./routes/user");
+// DB
 mongoose.connect(process.env.DATABASE_URI)
 .then(() => console.log("DB Connect!"))
 .catch((err) => console.log(`DB ERROR: ${err}!`));
-
+// CODE
+app.use("/api/users", userRoute);
+// SERVER
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on ${process.env.PORT}`);
 }); 
