0e526b355ce14aa2f353eb8debaceb54636b38ce
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-08-14 15:07:31 +0330

    setup database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.listen(5000, () => {
-    console.log("Server is running!");
-});
+mongoose.connect(process.env.DATABASE_URI)
+.then(() => console.log("DB Connect!"))
+.catch((err) => console.log(`DB ERROR: ${err}!`));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on ${process.env.PORT}`);
+}); 
