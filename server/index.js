29ead66390de23435476a944c4fc1101ef98f756
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-08-18 16:34:03 +0330

    setup products route

--- server/index.js
+++ server/index.js
@@ -7,13 +7,15 @@ dotenv.config();
 // ROUTES
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
 // DB
 mongoose.connect(process.env.DATABASE_URI)
-.then(() => console.log("DB Connect!"))
-.catch((err) => console.log(`DB ERROR: ${err}!`));
+    .then(() => console.log("DB Connect!"))
+    .catch((err) => console.log(`DB ERROR: ${err}!`));
 // CODE
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 // SERVER
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on ${process.env.PORT}`);
--- server/routes/user.js
+++ server/routes/user.js
@@ -6,8 +6,8 @@ const router = require("express").Router();
 router.get("/find/:id", verifyAdmin, async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
-        const { password, ...others } = user._doc;
-        res.status(200).json(others);
+        const { password, ...rest } = user._doc;
+        res.status(200).json(rest);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -17,7 +17,7 @@ router.get("/", verifyAdmin, async (req, res) => {
     const query = req.query.new;
     try {
         const users = query
-            ? await User.find().sort({ _id: -1 }).limit(5)
+            ? await User.find().sort({ createdAt: -1 }).limit(5)
             : await User.find();
         res.status(200).json(users);
     } catch (err) {
