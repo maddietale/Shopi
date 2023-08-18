b37b5a36bda609fd6cfb9c4ebc2fc2267cf0d08d
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-08-18 18:39:50 +0330

    add some changes

--- server/models/Product.js
+++ server/models/Product.js
@@ -23,17 +23,18 @@ const ProductSchema = new mongoose.Schema({
     },
     size:
     {
-        type: String
+        type: Array
     },
     color:
     {
-        type: String
+        type: Array
     },
     price:
     {
         type: Number,
         required: true
-    }
+    },
+    inStock: { type: Boolean, default: true }
 }, { timestamps: true });
 
 const Product = mongoose.model("Product", ProductSchema);
